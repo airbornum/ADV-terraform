92a82dafab48c6de30756163bf58f999f2fdc92b
Yevhen Khadzhamov <airbornum@gmail.com>
2020-03-11 13:12:43 +0200

    Add lesson-3 changing

--- lesson-3/vars.tf
+++ lesson-3/vars.tf
@@ -1,15 +1,15 @@
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
-  default = "us-east-1"
+  default = "eu-central-1"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
-    #    eu-central-1 = "ami-0c71ad703d12633ef"
+    us-east-1    = "ami-13be557e"
+    us-west-2    = "ami-06b94666"
+    eu-west-1    = "ami-0d729a60"
+    eu-central-1 = "ami-03a71cec707bfc3d7"
   }
 }
